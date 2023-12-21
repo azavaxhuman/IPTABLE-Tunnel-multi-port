19c5648681549491de5bfa3b9459a52aa51949ee
Azavax <21226881+azavaxhuman@users.noreply.github.com>
2023-12-21 16:27:15 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if running as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+# Install required packages
+echo "Installing required packages..."
+apt update
+apt install -y iptables iptables-persistent
+
+# Create symbolic link for iptables-dds script
+# به داخل پوشه مخزن می‌رویم
+cd /root/dds-tunnel
+
+# ساخت لینک نمادین با نام dds-tunnel که به tunnel.sh اشاره دارد
+ln -s "$(pwd)/tunnel.sh" /usr/local/bin/dds-tunnel
+
+# تغییر مجوزهای اجرایی اسکریپت
+chmod +x tunnel.sh
+
+
+echo "Installation completed. You can now use 'dds-tunnel' command."
